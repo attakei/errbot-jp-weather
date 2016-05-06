9c682023e78567d410cccdeecf22bfc78a5e2f44
attakei <attakei@gmail.com>
2016-05-06 09:38:32 +0900

    BeautifulSoupを用いて、起動時に都市情報を全部取得しておく

--- jpweather.py
+++ jpweather.py
@@ -21,40 +21,20 @@ class JpWeather(BotPlugin):
         """Say list of cities abled to report weather
         """
         if args == '':
-            cities = self.fetch_cities()
+            cities = [name for name in self['cities'].keys()]
         else:
-            cities = self.fetch_cities_from_area(args)
-        return u'\n'.join(['* {}'.format(city) for city in cities.keys()])
+            cities = [city['name'] for city in self['cities'].values() if city['area'] == args]
+        return u'\n'.join(cities)
 
     @botcmd
     def jpweather_area(self, msg, args):
         # area_name = args.split()[0]
-        areas = self.fetch_areas()
-        return u'\n'.join(['* {}'.format(area) for area in areas])
-
-    def find_city_id(self, city_name):
-        cities = self.fetch_cities()
-        return cities.get(city_name, None)
+        return u'\n'.join(self['areas'])
 
     def find_weather_hack(self, city_name):
-        city_id = self.find_city_id(city_name)
-        if city_id is None:
+        city = self['cities'].get(city_name, None)
+        if city_name is None:
             return u'{} は確認不可能な地域です'.format(city_name)
-        resp = requests.get(self.WEATHER_HACK_API_URL, {'city':city_id})
+        resp = requests.get(self.WEATHER_HACK_API_URL, {'city': city['id']})
         wt_json = resp.json()
         return u'{}: {}は{}'.format(wt_json['title'], wt_json['forecasts'][0]['dateLabel'], wt_json['forecasts'][0]['telop'])
-
-    def fetch_cities(self):
-        resp = requests.get(self.WEATHER_HACK_AREA_URL)
-        tree = ElementTree.fromstring(resp.content)
-        return {elm.attrib['title']: elm.attrib['id'] for elm in tree.findall('.//city')}
-
-    def fetch_areas(self):
-        resp = requests.get(self.WEATHER_HACK_AREA_URL)
-        tree = ElementTree.fromstring(resp.content)
-        return [elm.attrib['title'] for elm in tree.findall('.//pref')]
-
-    def fetch_cities_from_area(self, area):
-        resp = requests.get(self.WEATHER_HACK_AREA_URL)
-        tree = ElementTree.fromstring(resp.content)
-        return {elm.attrib['title']: elm.attrib['id'] for elm in tree.findall('./pref[title="' + area + '"]/city')}
